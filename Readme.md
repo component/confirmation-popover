ca8e7bdb1a88d70519a5c4a8cf90f1d7037749e5
TJ Holowaychuk <tj@vision-media.ca>
2012-08-06 12:35:10 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,62 @@
+
+# Confirmation Popover
+
+  Popover confirmation component built on top of [Popover](http://github.com/component/popover).
+
+  ![js popover confirmation component](http://f.cl.ly/items/2H3a3C1M1k071z1B360I/Screen%20Shot%202012-08-06%20at%2012.16.59%20PM.png)
+
+## Installation
+
+```
+$ npm install confirmation-popover-component
+```
+
+## Features
+
+  - all the features of Popover / Tip
+
+## Events
+
+  - `show` the confirmation is shown
+  - `hide` the confirmation is hidden
+  - `cancel` the user closed the confirmation or cancelled
+  - `ok` the user accepted
+
+## API
+
+### new ConfirmationPopover(msg, [title])
+
+  Create a new popover with `msg` and optional `title`.
+
+```js
+var Confirmation = require('confirmation-popover');
+var confirm = new Confirmation('This action cannot be undone.', 'Delete tobi?');
+confirm.show(el);
+```
+
+ View [Tip](http://github.com/component/tip) and [Popover](http://github.com/component/popover) for additional
+ API documentation.
+
+### ConfirmationPopover#focus(type)
+
+  By default the "cancel" button is focused, however you
+  may invoke `.focus('ok')`.
+
+### ConfirmationPopover#cancel(text)
+
+  Set cancel button `text`.
+
+### ConfirmationPopover#ok(text)
+
+  Set cancel ok `text`.
+
+### ConfirmationPopover#show(el, [fn])
+
+  Attach to `el`, and invoke `fn` with
+  a boolean representing the user's choice.
+
+  When `fn` is omitted you may still utilize the `cancel` / `ok` events.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,137 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Popover = require('popover')
+  , o = require('jquery');
+
+/**
+ * Expose `ConfirmationPopover`.
+ */
+
+module.exports = ConfirmationPopover;
+
+/**
+ * Initialize a `ConfirmationPopover` with the given `msg`
+ * and optional `title`.
+ *
+ * @param {Mixed} msg
+ * @param {Mixed} title
+ * @api public
+ */
+
+function ConfirmationPopover(msg, title) {
+  this.actions = o(render('popover'));
+  Popover.call(this, this.actions, title);
+  this.classname = 'popover confirmation-popover';
+  this.actions.find('.cancel').click(this.oncancel.bind(this));
+  this.actions.find('.ok').click(this.onok.bind(this));
+  this.message(msg);
+}
+
+/**
+ * Inherits from `Popover.prototype`.
+ */
+
+ConfirmationPopover.prototype.__proto__ = Popover.prototype;
+
+/**
+ * Handle cancel click.
+ *
+ * Emits "cancel".
+ *
+ * @param {Event} e
+ * @api private
+ */
+
+ConfirmationPopover.prototype.oncancel = function(e){
+  e.preventDefault();
+  this.emit('cancel');
+  this.callback(false);
+  this.hide();
+};
+
+/**
+ * Handle ok click.
+ *
+ * Emits "ok".
+ *
+ * @param {Event} e
+ * @api private
+ */
+
+ConfirmationPopover.prototype.onok = function(e){
+  e.preventDefault();
+  this.emit('ok');
+  this.callback(true);
+  this.hide();
+};
+
+/**
+ * Set confirmation `msg`.
+ *
+ * @param {String} msg
+ * @return {ConfirmationPopover}
+ * @api public
+ */
+
+ConfirmationPopover.prototype.message = function(msg){
+  this.actions.find('.confirmation-popover-message').text(msg);
+  return this;
+};
+
+/**
+ * Focus `type`, either "ok" or "cancel".
+ *
+ * @param {String} type
+ * @return {ConfirmationPopover}
+ * @api public
+ */
+
+ConfirmationPopover.prototype.focus = function(type){
+  this._focus = type;
+  return this;
+};
+
+/**
+ * Change "cancel" button `text`.
+ *
+ * @param {String} text
+ * @return {ConfirmationPopover}
+ * @api public
+ */
+
+ConfirmationPopover.prototype.cancel = function(text){
+  this.actions.find('.cancel').text(text);
+  return this;
+};
+
+/**
+ * Change "ok" button `text`.
+ *
+ * @param {String} text
+ * @return {ConfirmationPopover}
+ * @api public
+ */
+
+ConfirmationPopover.prototype.ok = function(text){
+  this.actions.find('.ok').text(text);
+  return this;
+};
+
+/**
+ * Show the tip attached to `el` and invoke `fn(ok)`.
+ *
+ * @param {jQuery|Element} el
+ * @param {Function} fn
+ * @return {ConfirmationPopover}
+ * @api public
+ */
+
+ConfirmationPopover.prototype.show = function(el, fn){
+  Popover.prototype.show.call(this, el);
+  this.el.find('.' + this._focus).focus();
+  this.callback = fn || function(){};
+  return this;
+};
